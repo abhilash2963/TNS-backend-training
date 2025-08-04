9f44013b79543ef63bcf709051f250a374120b0b
abhilash2963 <pitlaabhilash6@gmail.com>
2025-08-04 21:21:53 +0530

    initial commit

---
+++ helloworld/src/helloworld/Hello.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class Hello {
+	void print() {
+		System.out.println("WELCOME TO TNS");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello obj=new Hello();
+		obj.print();
+	}
+
+}
