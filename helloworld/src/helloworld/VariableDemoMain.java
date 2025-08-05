952114b93ab8be1ef1400a4003f144592c9385dd
abhilash2963 <pitlaabhilash6@gmail.com>
2025-08-05 22:15:35 +0530

    types of variables

---
+++ helloworld/src/helloworld/VariableDemoMain.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class VariableDemoMain {
+
+	public static void main(String[] args) {
+		
+		VariablesDemo obj=new VariablesDemo();
+		System.out.println(obj.name);
+		System.out.println(VariablesDemo.collageName);
+		obj.display();
+
+	}
+
+}
---
+++ helloworld/src/helloworld/VariablesDemo.java
@@ -0,0 +1,12 @@
+
+package helloworld;
+public class VariablesDemo {
+	String name="abhi";
+	static String collageName="KPRIT";
+	void display() {
+		
+		String nametwo="raj";
+		System.out.println(nametwo);
+	}
+
+}
